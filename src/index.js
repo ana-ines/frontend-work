9236a9e707c22ce85c87d126a1d108d15de5f4e0
Ana Inés Mardon <anitamardon@gmail.com>
2017-10-23 12:33:27 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const store = createStore(
 	reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 	applyMiddleware(epicMiddleware)
-)
+);
 
 render(<Provider store={ store }>
 				<App />
