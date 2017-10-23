de65e3c7944c6818ea76e5006c18c0a815c305eb
Ana Inés Mardon <anitamardon@gmail.com>
2017-10-23 12:43:19 -0300

    indentation

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ const epics = combineEpics(fetchDiskEpic, fetchDiskByNameEpic);
 const epicMiddleware = createEpicMiddleware(epics);
 
 const store = createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-	applyMiddleware(epicMiddleware)
-);
+								reducer,
+								window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+								applyMiddleware(epicMiddleware)
+							)
 
 render(<Provider store={ store }>
 				<App />
